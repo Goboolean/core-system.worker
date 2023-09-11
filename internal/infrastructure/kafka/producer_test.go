89dc5b3d95b7edee3d01dc320cc660694adcc8b6
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-09-11 21:39:54 +0900

    producer test code is now verified

--- internal/infrastructure/kafka/producer_test.go
+++ internal/infrastructure/kafka/producer_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/Goboolean/common/pkg/resolver"
+	model_latest "github.com/Goboolean/core-system.worker/api/kafka/model.latest"
 	"github.com/Goboolean/core-system.worker/internal/infrastructure/kafka"
 	"github.com/stretchr/testify/assert"
 )
@@ -25,6 +26,17 @@ func SetupProducer() *kafka.Producer {
 	return p
 }
 
+func SetupProducerWithRegistry() *kafka.Producer {
+	p, err := kafka.NewProducer(&resolver.ConfigMap{
+		"BOOTSTRAP_HOST": os.Getenv("KAFKA_BOOTSTRAP_HOST"),
+		"REGISTRY_HOST":  os.Getenv("KAFKA_REGISTRY_HOST"),
+	})
+	if err != nil {
+		panic(err)
+	}
+	return p
+}
+
 func TeardownProducer(p *kafka.Producer) {
 	p.Close()
 }
@@ -32,8 +44,14 @@ func TeardownProducer(p *kafka.Producer) {
 
 
 func Test_Producer(t *testing.T) {
+
 	p := SetupProducer()
 	defer TeardownProducer(p)
+
+	const topic = "default-topic"
+	var event = &model_latest.Event{
+		EventUuid: "test-uuid",
+	}
 
 	t.Run("Ping", func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
@@ -41,5 +59,64 @@ func Test_Producer(t *testing.T) {
 	
 		err := p.Ping(ctx)
 		assert.NoError(t, err)
+	})
+
+	t.Run("Register", func(t *testing.T) {	
+		_, err := p.Register(topic, &model_latest.Event{})
+		assert.NoError(t, err)
+	})
+
+	t.Run("ProduceMessage", func(t *testing.T) {
+		err := p.Produce(topic, event)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Flush", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		count, err := p.Flush(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, count)
+	})
+}
+
+
+func Test_ProducerWithRegistry(t *testing.T) {
+	t.Skip("Skip this test because of the registry is not ready.")
+
+	p := SetupProducerWithRegistry()
+	defer TeardownProducer(p)
+
+	const topic = "default-topic"
+	var event = &model_latest.Event{
+		EventUuid: "test-uuid",
+	}
+
+	t.Run("Ping", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+	
+		err := p.Ping(ctx)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Register", func(t *testing.T) {	
+		_, err := p.Register(topic, &model_latest.Event{})
+		assert.NoError(t, err)
+	})
+
+	t.Run("ProduceMessage", func(t *testing.T) {
+		err := p.Produce(topic, event)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Flush", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		count, err := p.Flush(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, count)
 	})
 }
