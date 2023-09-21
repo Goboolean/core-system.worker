020b4678c38ae8d0c63b96123ab294f302e6bd9b
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-09-21 23:44:19 +0900

    node list for perfect test coverage

--- internal/infrastructure/kafka/main_test.go
+++ internal/infrastructure/kafka/main_test.go
@@ -11,6 +11,15 @@ import (
 
 var mutex = &sync.Mutex{}
 
+// List for perfect test coverage
+// [*] 1. Ping test
+// [*] 2. Produce and consume
+// [ ] 3. Produce and consume not existing topic
+// [*] 4. Consume with different group
+// [*] 5. Consume with same group
+// [ ] 6. Consume with registry
+// [ ] 7. Consume invalid typed message
+
 
 
 func TestMain(m *testing.M) {
