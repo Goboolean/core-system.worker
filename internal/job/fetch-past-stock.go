a28ecaed1b3248712a28e6d060cb41e6b05a2c72
orca <lsjtop10@gmail.com>
2024-03-29 18:20:53 +0900

    Fix PastStockFetcher
    Chane fetcher not to return error chan
    Remove unnecessary syntax

--- internal/job/fetch-past-stock.go
+++ internal/job/fetch-past-stock.go
@@ -27,7 +27,6 @@ type PastStockFetcher struct {
 	in  chan any `type:"none"`
 	out chan any `type:"*StockAggregate"` //Job은 자신의 Output 채널에 대해 소유권을 가진다.
 
-	err chan error
 }
 
 func NewPastStockFetcher(mongo infrastructure.MongoClientStock, parmas *UserParams) (*PastStockFetcher, error) {
@@ -38,7 +37,6 @@ func NewPastStockFetcher(mongo infrastructure.MongoClientStock, parmas *UserPara
 		pastRepo:            mongo,
 		isFetchingFullRange: true,
 		out:                 make(chan any),
-		err:                 make(chan error),
 	}
 
 	if !parmas.IsKeyNullOrEmpty("stockId") {
@@ -66,61 +64,49 @@ func NewPastStockFetcher(mongo infrastructure.MongoClientStock, parmas *UserPara
 	return instance, err
 }
 
-func (f *PastStockFetcher) Execute(ctx context.Context) chan error {
-	errChan := make(chan error)
+func (f *PastStockFetcher) Execute(ctx context.Context) {
 	go func() {
 		defer close(f.out)
-		defer close(errChan)
-		for {
-			select {
-			case <-ctx.Done():
-				//외부에서 종료 처리가 왔을 때 처리
-				return
-			default:
-				f.pastRepo.SetTarget(f.stockId, f.timeSlice)
-				//가져올 데이터의 개수
-				var quantity int
-				//처음 가져올 데이터의 Index
-				var index int
-				var err error
-				var count int = f.pastRepo.GetCount(ctx)
 
-				if f.isFetchingFullRange {
-					index = 0
-					quantity = count
-				} else {
+		f.pastRepo.SetTarget(f.stockId, f.timeSlice)
+		//가져올 데이터의 개수
+		var quantity int
+		//처음 가져올 데이터의 Index
+		var index int
+		var err error
+		var count int = f.pastRepo.GetCount(ctx)
 
-					index, err = f.pastRepo.FindLatestIndexBy(ctx, f.startTimestamp)
-					if err != nil {
-						panic(err)
-					}
+		if f.isFetchingFullRange {
+			index = 0
+			quantity = count
+		} else {
 
-					//1,2,3,4,5
-					quantity = count - index
-				}
-
-				duration, _ := time.ParseDuration(f.timeSlice)
-				err = f.pastRepo.ForEachDocument(ctx, index, quantity, func(doc infrastructure.StockDocument) {
-					f.out <- &dto.StockAggregate{
-						OpenTime:   doc.Timestamp,
-						ClosedTime: doc.Timestamp + (duration.Milliseconds() / 1000),
-						Open:       doc.Open,
-						Closed:     doc.Close,
-						High:       doc.High,
-						Low:        doc.Low,
-						Volume:     float32(doc.Volume),
-					}
-				})
-				if err != nil {
-					panic(err)
-				}
-
-				return
+			index, err = f.pastRepo.FindLatestIndexBy(ctx, f.startTimestamp)
+			if err != nil {
+				panic(err)
 			}
 
+			//1,2,3,4,5
+			quantity = count - index
 		}
+
+		duration, _ := time.ParseDuration(f.timeSlice)
+		err = f.pastRepo.ForEachDocument(ctx, index, quantity, func(doc infrastructure.StockDocument) {
+			f.out <- &dto.StockAggregate{
+				OpenTime:   doc.Timestamp,
+				ClosedTime: doc.Timestamp + (duration.Milliseconds() / 1000),
+				Open:       doc.Open,
+				Closed:     doc.Close,
+				High:       doc.High,
+				Low:        doc.Low,
+				Volume:     float32(doc.Volume),
+			}
+		})
+		if err != nil {
+			panic(err)
+		}
+
 	}()
-	return errChan
 }
 
 func (j *PastStockFetcher) SetInputChan(input chan any) {
