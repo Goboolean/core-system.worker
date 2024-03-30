f87e74bf861b3eafdc2e9a4181dfc897e53b4a1b
orca <lsjtop10@gmail.com>
2024-03-30 11:31:30 +0900

    Test: add past fetch stock test

--- internal/job/fetch-past-stock_test.go
+++ internal/job/fetch-past-stock_test.go
@@ -1,7 +1,130 @@
 package job_test
 
-import "testing"
+import (
+	"context"
+	"sync"
+	"testing"
 
-func TestMain(m *testing.M) {
+	"github.com/Goboolean/common/pkg/resolver"
+	"github.com/Goboolean/core-system.worker/internal/dto"
+	"github.com/Goboolean/core-system.worker/internal/infrastructure"
+	"github.com/Goboolean/core-system.worker/internal/infrastructure/mock"
+	"github.com/Goboolean/core-system.worker/internal/job"
+	"github.com/stretchr/testify/assert"
+)
 
+func TestPastStock(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+
+		mongo, _ := mock.NewMongoClientStock(&resolver.ConfigMap{}, []*infrastructure.StockDocument{
+			&infrastructure.StockDocument{
+				Symbol:    "aapl",
+				Open:      12,
+				Close:     150,
+				High:      150,
+				Low:       23,
+				Average:   0,
+				Volume:    12,
+				Timestamp: 1711758929,
+			},
+			&infrastructure.StockDocument{
+				Symbol:    "aapl",
+				Open:      12,
+				Close:     150,
+				High:      150,
+				Low:       23,
+				Average:   0,
+				Volume:    12,
+				Timestamp: 1711759229,
+			},
+			&infrastructure.StockDocument{
+				Symbol:    "aapl",
+				Open:      12,
+				Close:     150,
+				High:      150,
+				Low:       23,
+				Average:   0,
+				Volume:    12,
+				Timestamp: 1711759529,
+			},
+			&infrastructure.StockDocument{
+				Symbol:    "aapl",
+				Open:      12,
+				Close:     150,
+				High:      150,
+				Low:       23,
+				Average:   0,
+				Volume:    12,
+				Timestamp: 1711759829,
+			},
+		})
+
+		ctx, _ := context.WithCancel(context.TODO())
+
+		fetch, err := job.NewPastStockFetcher(mongo, &job.UserParams{"timeslice": "1m"})
+		if err != nil {
+			t.Error(err)
+		}
+
+		fetch.Execute(ctx)
+		out := fetch.OutputChan()
+		res := []*dto.StockAggregate{}
+
+		wg := sync.WaitGroup{}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for data := range out {
+				val, ok := data.(*dto.StockAggregate)
+				if !ok {
+					panic("Type miss match")
+				}
+				res = append(res, val)
+			}
+		}()
+
+		wg.Wait()
+
+		exp := []*dto.StockAggregate{
+			&dto.StockAggregate{
+				OpenTime:   1711758929,
+				ClosedTime: 1711758989,
+				Open:       12,
+				Closed:     150,
+				High:       150,
+				Low:        23,
+				Volume:     12,
+			},
+			&dto.StockAggregate{
+				OpenTime:   1711759229,
+				ClosedTime: 1711759289,
+				Open:       12,
+				Closed:     150,
+				High:       150,
+				Low:        23,
+				Volume:     12,
+			},
+			&dto.StockAggregate{
+				OpenTime:   1711759529,
+				ClosedTime: 1711759589,
+				Open:       12,
+				Closed:     150,
+				High:       150,
+				Low:        23,
+				Volume:     12,
+			},
+			&dto.StockAggregate{
+				OpenTime:   1711759829,
+				ClosedTime: 1711759889,
+				Open:       12,
+				Closed:     150,
+				High:       150,
+				Low:        23,
+				Volume:     12,
+			},
+		}
+
+		assert.Equal(t, exp, res)
+	})
 }
