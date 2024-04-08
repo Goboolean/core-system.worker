6d886500bcb5b4ce83c6498fc3ef478355ffa1d6
orca <lsjtop10@gmail.com>
2024-04-08 15:23:04 +0900

    Fix type of Data of InferenceReq struct

--- internal/dto/Kserve/request.go
+++ internal/dto/Kserve/request.go
@@ -2,9 +2,9 @@ package kserve
 
 // InferenceReq는 kserve에 요청을 보내기 위한 구조체입니다.
 type InferenceReq struct {
-	Name       string        `json:"string"`
-	Shape      []int         `json:"shape"`
-	DataType   string        `json:"datatype"`
-	Parameters string        `json:"parameters,omitempty"`
-	Data       []interface{} `json:"data"`
+	Name       string      `json:"string"`
+	Shape      []int       `json:"shape"`
+	DataType   string      `json:"datatype"`
+	Parameters string      `json:"parameters,omitempty"`
+	Data       interface{} `json:"data"`
 }
