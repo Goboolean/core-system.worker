74677e61fcace3a141511d0f067ae197adb9c064
orca <lsjtop10@gmail.com>
2024-04-08 13:40:05 +0900

    Write infrastructure

---
+++ internal/infrastructure/kserve.go
@@ -0,0 +1,17 @@
+package infrastructure
+
+import "github.com/Goboolean/common/pkg/resolver"
+
+type KServeClientImpl struct {
+}
+
+// KServeClient는 KServe에 요청을 보내고 받는 역할을 하는 인터페이스입니다.
+type KServeClient interface {
+	SetModelName(name string)
+	AddFloatModelParam(key string, value float32) error
+	RequestInference(shape []int, input []float32) (output []float32, err error)
+}
+
+func NewKServeClient(c *resolver.ConfigMap) *KServeClientImpl {
+	return &KServeClientImpl{}
+}
