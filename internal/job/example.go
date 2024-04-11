d488870265078938dca5f3a83a2c26473c2e332f
orca <lsjtop10@gmail.com>
2024-04-11 13:58:53 +0900

    Fix: fix example job

--- internal/job/example.go
+++ internal/job/example.go
@@ -1,7 +1,10 @@
 package job
 
 /*
-import "context"
+import (
+	"context"
+	"strconv"
+)
 
 type ExampleJob struct {
 	Job
@@ -13,7 +16,7 @@ type ExampleJob struct {
 	out chan any `type:` //Job은 자신의 Output 채널에 대해 소유권을 가진다.
 }
 
-func NewExampleJob(params UserParams) *ExampleJob {
+func NewExampleJob(params UserParams) (*ExampleJob, error) {
 	//여기에 기본값 입력 아웃풋 채널은 job이 소유권을 가져야 한다.
 	instance := &ExampleJob{
 		out: make(chan any),
@@ -21,28 +24,37 @@ func NewExampleJob(params UserParams) *ExampleJob {
 
 	//여기에서 user param 초기화
 	if param1, ok := params["param1"]; ok {
-		instance.param1 = param1
+
+		val, err := strconv.ParseFloat(param1, 32)
+		if err != nil {
+			return nil, err
+		}
+
+		instance.param1 = float32(val)
 	}
 
-	return instance
+	return instance, nil
 }
 
 func (j *ExampleJob) Execute(ctx context.Context) {
-	defer close(j.out)
 	go func() {
-		select {
-		case <-ctx.Done():
-			//종료 처리가 왔을 때 처리
-		case input, ok := <-j.in:
-			if !ok {
-				//입력 채널이 닫혔을 때 처리
-				return
-			}
+		for {
+			defer close(j.out)
+			select {
+			case <-ctx.Done():
+				//종료 처리가 왔을 때 처리
+			case input, ok := <-j.in:
+				if !ok {
+					//입력 채널이 닫혔을 때 처리
+					return
+				}
 
-			data := input.(int)
-			//데이터를 이용한 처리
-			j.out <- data
+				data := input.(int)
+				//데이터를 이용한 처리
+				j.out <- data
+			}
 		}
+
 	}()
 
 }
@@ -54,5 +66,4 @@ func (j *ExampleJob) SetInputChan(input chan any) {
 func (j *ExampleJob) OutputChan() chan any {
 	return j.out
 }
-
 */
