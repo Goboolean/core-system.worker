453ca02122a4b866849cd4a04af5396bf7bf7f6e
orca <lsjtop10@gmail.com>
2024-04-11 09:56:13 +0900

    Fix: fix KServeClient Interface
    Fix RequestInference to recieve ctx

--- internal/infrastructure/kserve.go
+++ internal/infrastructure/kserve.go
@@ -8,8 +8,7 @@ type KServeClientImpl struct {
 // KServeClient는 KServe에 요청을 보내고 받는 역할을 하는 인터페이스입니다.
 type KServeClient interface {
 	SetModelName(name string)
-	AddFloatModelParam(key string, value float32) error
-	RequestInference(shape []int, input []float32) (output []float32, err error)
+	RequestInference(ctx context.Context, shape []int, input []float32) (output []float32, err error)
 }
 
 func NewKServeClient(c *resolver.ConfigMap) *KServeClientImpl {
