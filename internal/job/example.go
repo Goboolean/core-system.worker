4e75d7fe768e913357a7df0f1d9492ea0c6a6916
orca <35363038+lsjtop10@users.noreply.github.com>
2024-04-16 20:51:23 +0900

    Fix example job (#43)
    * Fix: fix example job
    
    * Fix: apply graceful stop

--- internal/job/example.go
+++ internal/job/example.go
@@ -1,7 +1,10 @@
 package job
 
-/*
-import "context"
+import (
+	"context"
+	"strconv"
+	"sync"
+)
 
 type ExampleJob struct {
 	Job
@@ -11,9 +14,10 @@ type ExampleJob struct {
 
 	in  chan any `type:`
 	out chan any `type:` //Job은 자신의 Output 채널에 대해 소유권을 가진다.
+	wg  sync.WaitGroup
 }
 
-func NewExampleJob(params UserParams) *ExampleJob {
+func NewExampleJob(params UserParams) (*ExampleJob, error) {
 	//여기에 기본값 입력 아웃풋 채널은 job이 소유권을 가져야 한다.
 	instance := &ExampleJob{
 		out: make(chan any),
@@ -21,28 +25,40 @@ func NewExampleJob(params UserParams) *ExampleJob {
 
 	//여기에서 user param 초기화
 	if param1, ok := params["param1"]; ok {
-		instance.param1 = param1
+
+		val, err := strconv.ParseFloat(param1, 32)
+		if err != nil {
+			return nil, err
+		}
+
+		instance.param1 = float32(val)
 	}
 
-	return instance
+	return instance, nil
 }
 
 func (j *ExampleJob) Execute(ctx context.Context) {
-	defer close(j.out)
+	j.wg.Add(1)
 	go func() {
-		select {
-		case <-ctx.Done():
-			//종료 처리가 왔을 때 처리
-		case input, ok := <-j.in:
-			if !ok {
-				//입력 채널이 닫혔을 때 처리
-				return
-			}
+		defer j.wg.Done()
+		defer close(j.out)
+		for {
 
-			data := input.(int)
-			//데이터를 이용한 처리
-			j.out <- data
+			select {
+			case <-ctx.Done():
+				//종료 처리가 왔을 때 처리
+			case input, ok := <-j.in:
+				if !ok {
+					//입력 채널이 닫혔을 때 처리
+					return
+				}
+
+				data := input.(int)
+				//데이터를 이용한 처리
+				j.out <- data
+			}
 		}
+
 	}()
 
 }
@@ -55,4 +71,6 @@ func (j *ExampleJob) OutputChan() chan any {
 	return j.out
 }
 
-*/
+func (j *ExampleJob) Close() {
+	j.wg.Wait()
+}
