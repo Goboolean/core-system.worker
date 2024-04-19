c22b2ecb099313fa72c2cb06567087961504d6b2
lsjtop10 <lsjtop10@gmail.com>
2024-04-19 11:20:03 +0900

    Refactor: refactor infrastructure package structure

--- internal/dto/kserve.go
+++ internal/dto/kserve.go
@@ -1,4 +1,13 @@
-package kserve
+package dto
+
+// InferenceReq는 kserve에 요청을 보내기 위한 구조체입니다.
+type InferenceReq struct {
+	Name       string      `json:"string"`
+	Shape      []int       `json:"shape"`
+	DataType   string      `json:"datatype"`
+	Parameters string      `json:"parameters,omitempty"`
+	Data       interface{} `json:"data"`
+}
 
 // InferenceRes는 kserve로부터 받은 응답을 담기 위한 구조체입니다.
 type InferenceRes struct {
