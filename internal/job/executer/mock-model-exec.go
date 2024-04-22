ce53015ca729edb6895026d8ab7c220421344d5c
orca <35363038+lsjtop10@users.noreply.github.com>
2024-04-22 16:47:28 +0900

    Fix: apply graceful stop (#46)
    * Fix: apply graceful stop
    
    * fix typo
    
    apply lint (#53)

--- internal/job/executer/mock-model-exec.go
+++ internal/job/executer/mock-model-exec.go
@@ -126,11 +126,11 @@ func (m *Mock) Execute(ctx context.Context) {
 
 }
 
-func (m *Mock) SetInputChan(input chan any) {
+func (m *Mock) SetInput(input chan any) {
 	m.in = input
 }
 
-func (m *Mock) OutputChan() chan any {
+func (m *Mock) Output() chan any {
 	return m.out
 }
 
