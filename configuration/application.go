25cc9e4d41bfc60b91db5c512a74c96f84afa857
lsjtop10 <lsjtop10@gmail.com>
2024-04-30 16:54:59 +0900

    Add application configuration struct

---
+++ configuration/application.go
@@ -0,0 +1,32 @@
+package configuration
+
+type AppConfig struct {
+	Task       string         `yaml:"task"`
+	DataOrigin DataOrigin     `yaml:"dataOrigin"`
+	Model      ModelConfig    `yaml:"model"`
+	Strategy   StrategyConfig `yaml:"strategy"`
+}
+
+type DataOrigin struct {
+	TimeFrame      TimeFrame `yaml:"timeFrame"`
+	ProductType    string    `yaml:"productType"`
+	StartTimestamp int64     `yaml:"startTimestamp"`
+	EndTimestamp   int64     `yaml:"endTimestamp"`
+}
+
+type TimeFrame struct {
+	Seconds int `yaml:"seconds"`
+}
+
+type ModelConfig struct {
+	Id         string             `yaml:"id"`
+	BatchSize  int                `yaml:"batchSize"`
+	OutputType string             `yaml:"outputType"`
+	Parmas     map[string]float32 `yaml:"params"`
+}
+
+type StrategyConfig struct {
+	Id        string             `yaml:"id"`
+	InputType string             `yaml:"inputType"`
+	Params    map[string]float32 `yaml:"params"`
+}
