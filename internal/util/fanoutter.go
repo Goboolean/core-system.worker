a18c36f00c60a25868b8d64b2c3042db51beff4f
orca <lsjtop10@gmail.com>
2024-05-13 14:29:04 +0900

    feat: implement fanoutter

---
+++ internal/util/fanoutter.go
@@ -0,0 +1,44 @@
+package util
+
+type Fanoutter struct {
+	in  chan any
+	out []chan any
+
+	stop chan struct{}
+}
+
+func (fo *Fanoutter) SetInput(in chan any) {
+	fo.in = in
+}
+
+func (fo *Fanoutter) Output() chan any {
+	ch := make(chan any, 1)
+	fo.out = append(fo.out, ch)
+	return ch
+}
+
+func (fo *Fanoutter) Execute() {
+	go func() {
+		defer func() {
+			for _, ch := range fo.out {
+				close(ch)
+			}
+		}()
+
+		select {
+		case <-fo.stop:
+			return
+		case data, ok := <-fo.in:
+			if !ok {
+				close(fo.stop)
+				return
+			}
+
+			for _, ch := range fo.out {
+				ch <- data
+			}
+		}
+
+	}()
+
+}
