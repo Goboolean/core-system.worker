efc9a19d1e59c537d8ce5b77ee81267a23833c1d
orca <35363038+lsjtop10@users.noreply.github.com>
2024-05-14 09:36:36 +0900

    Refactor: mode dto to model package (#70)

--- internal/model/kserve.go
+++ internal/model/kserve.go
@@ -1,4 +1,4 @@
-package dto
+package model
 
 // InferenceReq는 kserve에 요청을 보내기 위한 구조체입니다.
 type InferenceReq struct {
--- internal/model/stock_agregate.go
+++ internal/model/stock_agregate.go
@@ -1,4 +1,4 @@
-package dto
+package model
 
 // stockAggreate 우리
 type StockAggregate struct {
