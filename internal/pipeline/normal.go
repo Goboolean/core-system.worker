74b52190ade0206942be3a70fba8de89333f48c1
orca <35363038+lsjtop10@users.noreply.github.com>
2024-05-15 09:13:54 +0900

    Feat: Add joiner to normal pipeline (#75)
    * Feat: Add joiner to normal pipeline
    
    * Feat: add wiring and nil check logic

--- internal/pipeline/normal.go
+++ internal/pipeline/normal.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"reflect"
 
 	"github.com/Goboolean/core-system.worker/internal/job/adapter"
 	"github.com/Goboolean/core-system.worker/internal/job/analyzer"
 	"github.com/Goboolean/core-system.worker/internal/job/executer"
 	"github.com/Goboolean/core-system.worker/internal/job/fetcher"
+	"github.com/Goboolean/core-system.worker/internal/job/joinner"
 	"github.com/Goboolean/core-system.worker/internal/job/transmitter"
+	"github.com/Goboolean/core-system.worker/internal/util"
 )
 
 var ErrTypeNotMatch = errors.New("pipeline: cannot build a pipeline because the types are not compatible between the jobs")
@@ -20,10 +23,12 @@ type Normal struct {
 	fetch      fetcher.Fetcher
 	modelExec  executer.ModelExecutor
 	adapt      adapter.Adapter
+	join       joinner.Joinner
 	resAnalyze analyzer.Analyzer
 	transmit   transmitter.Transmitter
 
 	//utils
+	mux util.ChannelMux
 
 	ctx    context.Context
 	cancel context.CancelFunc
@@ -33,6 +38,7 @@ func newNormalWithAdapter(
 	fetch fetcher.Fetcher,
 	modelExec executer.ModelExecutor,
 	adapt adapter.Adapter,
+	join joinner.Joinner,
 	resAnalyze analyzer.Analyzer,
 	transmit transmitter.Transmitter) (*Normal, error) {
 
@@ -40,9 +46,20 @@ func newNormalWithAdapter(
 		fetch:      fetch,
 		modelExec:  modelExec,
 		adapt:      adapt,
+		join:       join,
 		resAnalyze: resAnalyze,
 		transmit:   transmit,
+
+		mux: util.ChannelMux{},
 	}
+
+	instance.mux.SetInput(instance.fetch.Output())
+	instance.modelExec.SetInput(instance.mux.Output())
+	adapt.SetInput(instance.modelExec.Output())
+	instance.join.SetModelInput(instance.adapt.Output())
+	instance.join.SetRefInput(instance.mux.Output())
+	instance.resAnalyze.SetInput(instance.join.Output())
+	instance.transmit.SetInput(instance.resAnalyze.Output())
 
 	return &instance, nil
 }
@@ -50,47 +67,56 @@ func newNormalWithAdapter(
 func newNormalWithoutAdapter(
 	fetch fetcher.Fetcher,
 	modelExec executer.ModelExecutor,
+	join joinner.Joinner,
 	resAnalyze analyzer.Analyzer,
 	transmit transmitter.Transmitter) (*Normal, error) {
 
 	instance := Normal{
 		fetch:      fetch,
 		modelExec:  modelExec,
+		join:       join,
 		resAnalyze: resAnalyze,
 		transmit:   transmit,
 	}
+
+	instance.mux.SetInput(instance.fetch.Output())
+	instance.modelExec.SetInput(instance.mux.Output())
+	instance.join.SetModelInput(instance.modelExec.Output())
+	instance.join.SetRefInput(instance.mux.Output())
+	instance.resAnalyze.SetInput(instance.join.Output())
+	instance.transmit.SetInput(instance.resAnalyze.Output())
 
 	return &instance, nil
 
 }
 
-func (p *Normal) Run() {
-	p.ctx, p.cancel = context.WithCancel(context.Background())
-	//TODO: 하나의 컴포넌트에서 발행한 메시지를
+func (n *Normal) Run() {
 
-	p.fetch.Execute()
-	p.modelExec.Execute()
-	p.adapt.Execute()
-	p.resAnalyze.Execute()
-	p.transmit.Execute()
-
+	n.fetch.Execute()
+	n.modelExec.Execute()
+	if !reflect.ValueOf(n.adapt).IsNil() {
+		n.adapt.Execute()
+	}
+	n.join.Execute()
+	n.resAnalyze.Execute()
+	n.transmit.Execute()
 }
 
-func (p *Normal) Stop() error {
+func (n *Normal) Stop() error {
 
-	if err := p.fetch.Close(); err != nil {
+	if err := n.fetch.Close(); err != nil {
 		return fmt.Errorf("pipeline: failed to shutdown fetch job %w", err)
 	}
-	if err := p.modelExec.Close(); err != nil {
+	if err := n.modelExec.Close(); err != nil {
 		return fmt.Errorf("pipeline: failed to shutdown model execute job %w", err)
 	}
-	if err := p.adapt.Close(); err != nil {
+	if err := n.adapt.Close(); err != nil {
 		return fmt.Errorf("pipeline: failed to shutdown adapt job %w", err)
 	}
-	if err := p.resAnalyze.Close(); err != nil {
+	if err := n.resAnalyze.Close(); err != nil {
 		return fmt.Errorf("pipeline: failed to shutdown analyze job %w", err)
 	}
-	if err := p.transmit.Close(); err != nil {
+	if err := n.transmit.Close(); err != nil {
 		return fmt.Errorf("pipeline: failed to shutdown transmit job %w", err)
 	}
 
