7964c54186e6b9bd6ecc5fcbe932f11d91ee684b
orca <lsjtop10@gmail.com>
2024-06-03 17:02:29 +0900

    Chore: Fix naming convention and Fix comment

--- internal/job/fetcher/interface.go
+++ internal/job/fetcher/interface.go
@@ -4,47 +4,46 @@ import (
 	"github.com/Goboolean/core-system.worker/internal/job"
 )
 
+// Fetcher is an interface that represents a job fetcher.
 type Fetcher interface {
 	job.Common
 
+	// Output returns the data channel for the fetched job data.
 	Output() job.DataChan
 }
 
-// FetchingSessin represents a session for fetching data points.
+// FetchingSessin is an interface that represents a fetching session.
 type FetchingSessin interface {
-	// TryNext attempts to move to the next data point in the session.
-	// It returns true if there is a next data point, false otherwise.
+	// TryNext returns true if there is another session to try.
 	TryNext() bool
 
-	// Next returns the next FetchingSessin and an error, if any.
+	// Next returns the next fetching session and an error, if any.
 	Next() (FetchingSessin, error)
 
-	// DecodedValue decodes the value of the data point into the provided variable.
-	// It returns an error if the decoding fails.
+	// DecodedValue decodes the value into the provided variable.
 	DecodedValue(v any) error
 
+	// Close closes the fetching session.
 	Close() error
 }
 
-// Fetcher is an interface that defines methods for fetching data from InfluxDB.
-type TradeReposityPort interface {
-
-	// SelectProduct selects a product by its ID and time frame.
+// TradeReposity is an interface that represents a trade repository.
+type TradeReposity interface {
+	// SelectProduct selects a product by ID, time frame, and product type.
 	SelectProduct(ID string, timeFrame string, productType string)
 
 	// SetRangeByTime sets the time range for fetching data.
 	SetRangeByTime(fromTimestamp int64, toTimestamp int64)
 
-	// Session returns a fetching session for executing the fetch operation.
+	// Session returns a fetching session.
 	Session() FetchingSessin
 }
 
-// Fetcher is an interface that defines methods for fetching data from InfluxDB.
-type TradeStreamPort interface {
-
-	// SelectProduct selects a product by its ID and time frame.
+// TradeStream is an interface that represents a trade stream.
+type TradeStream interface {
+	// SelectProduct selects a product by ID, time frame, and product type.
 	SelectProduct(ID string, timeFrame string, productType string)
 
-	// Session returns a fetching session for executing the fetch operation.
+	// Session returns a fetching session.
 	Session() FetchingSessin
 }
