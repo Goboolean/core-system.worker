0f11f9ba23e43175ed14b1a46a3a937f32b35129
orca <35363038+lsjtop10@users.noreply.github.com>
2024-06-03 10:55:25 +0900

    Fix/joiner (#98)
    * Fix: fix out of range error in joinner
    
    * Test: move for syntex inside

--- internal/job/joiner/by_sequnce_num_test.go
+++ internal/job/joiner/by_sequnce_num_test.go
@@ -15,85 +15,89 @@ func TestMain(m *testing.M) {
 
 func TestJoinBySequnceNum(t *testing.T) {
 	t.Run("Sequnce가 같은 두 데이터가 주어졌을 때, 이 두 데이터를 join해서 출력해야 한다.", func(t *testing.T) {
-		//arrange
-		refrenceInput := []model.Packet{
-			{
-				Sequnce: 1,
-				Data:    1,
-			},
-			{
-				Sequnce: 2,
-				Data:    1,
-			},
-			{
-				Sequnce: 3,
-				Data:    1,
-			},
-		}
 
-		modelInput := []model.Packet{
-
-			{
-				Sequnce: 2,
-				Data:    2,
-			},
-			{
-				Sequnce: 3,
-				Data:    2,
-			},
-		}
-
-		exp := []model.Packet{
-			{
-				Sequnce: 2,
-				Data: &model.Pair{
-					RefData:   1,
-					ModelData: 2,
+		for i := 0; i < 100; i++ {
+			//arrange
+			refrenceInput := []model.Packet{
+				{
+					Sequnce: 1,
+					Data:    1,
 				},
-			},
-			{
-				Sequnce: 3,
-				Data: &model.Pair{
-					RefData:   1,
-					ModelData: 2,
+				{
+					Sequnce: 2,
+					Data:    1,
 				},
-			},
-		}
-
-		refrenceInputChan := make(job.DataChan)
-		modelInputChan := make(job.DataChan)
-
-		go func() {
-			defer close(refrenceInputChan)
-			for _, e := range refrenceInput {
-				refrenceInputChan <- e
+				{
+					Sequnce: 3,
+					Data:    1,
+				},
 			}
-		}()
 
-		go func() {
-			defer close(modelInputChan)
-			for _, e := range modelInput {
-				modelInputChan <- e
+			modelInput := []model.Packet{
+
+				{
+					Sequnce: 2,
+					Data:    2,
+				},
+				{
+					Sequnce: 3,
+					Data:    2,
+				},
 			}
-		}()
 
-		joinner, err := joiner.NewBysequnce(&job.UserParams{})
-		if err != nil {
-			t.Error(err)
-			return
+			exp := []model.Packet{
+				{
+					Sequnce: 2,
+					Data: &model.Pair{
+						RefData:   1,
+						ModelData: 2,
+					},
+				},
+				{
+					Sequnce: 3,
+					Data: &model.Pair{
+						RefData:   1,
+						ModelData: 2,
+					},
+				},
+			}
+
+			refrenceInputChan := make(job.DataChan)
+			modelInputChan := make(job.DataChan)
+
+			go func() {
+				defer close(refrenceInputChan)
+				for _, e := range refrenceInput {
+					refrenceInputChan <- e
+				}
+			}()
+
+			go func() {
+				defer close(modelInputChan)
+				for _, e := range modelInput {
+					modelInputChan <- e
+				}
+			}()
+
+			joinner, err := joiner.NewBysequnce(&job.UserParams{})
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			joinner.SetRefInput(refrenceInputChan)
+			joinner.SetModelInput(modelInputChan)
+			outputChan := joinner.Output()
+
+			//act
+			output := make([]model.Packet, 0)
+			joinner.Execute()
+			for e := range outputChan {
+				output = append(output, e)
+			}
+			//assert
+			assert.Equal(t, exp, output)
 		}
-
-		joinner.SetRefInput(refrenceInputChan)
-		joinner.SetModelInput(modelInputChan)
-		outputChan := joinner.Output()
-
-		//act
-		output := make([]model.Packet, 0)
-		joinner.Execute()
-		for e := range outputChan {
-			output = append(output, e)
-		}
-		//assert
-		assert.Equal(t, exp, output)
 	})
+
 }
