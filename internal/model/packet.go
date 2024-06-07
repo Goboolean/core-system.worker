f4ee2f149c108bed1bc20021b0076dbb202b0d2e
orca <lsjtop10@gmail.com>
2024-06-07 11:45:43 +0900

    Docs: Add comment to model

--- internal/model/packet.go
+++ internal/model/packet.go
@@ -1,7 +1,13 @@
 package model
 
+// Packet은 Job과 Job 사이에서 데이터를 전달할 때 사용하는 구조체입니다.
+// Job과 Job 사이에서 교환될 데이터와 그 메타 정보를 포합합니다.
 type Packet struct {
+	// Sequence는 데이터가 생성된 순서를 나타내는 변수입니다.
+	// Job과 Job사이를 이동할 때 sequence는 변하지 않습니다.
 	Sequence int64
-	//type ?
+
+	// Data는 원하는 데이터를 타입에 관계없이 아무거나 보관할 수 있습니다.
+	// 이때 Data에 구조체를 싣고 싶은 경우 포인터 타입으로 실어야 합니다.
 	Data any
 }
--- internal/util/stop_notifier.go
+++ internal/util/stop_notifier.go
@@ -11,8 +11,18 @@ func init() {
 	close(closedChan)
 }
 
+// StopNotifier is a structure that manages stop.
+// It can be used to notify stop to goroutine.
+//
+// It MUST be instantiated using NewStopNotifier().
 type StopNotifier struct {
 	mu sync.Mutex
+
+	// The reason for using a channel in this structure is due to the unique properties of channels.
+	// When a channel is open, the receiver's code flow is blocked until data is sent to the channel.
+	// That is way the channel is not selected in a select-case statement.
+	// When a channel is closed, it returns a zero value to the receiver and does not block the receiver's code flow.
+	//That is way the channel is selected in a select-case statement.
 	ch chan struct{}
 }
 
@@ -22,13 +32,21 @@ func NewStopNotifier() *StopNotifier {
 	}
 }
 
+// NotifyStop notifies the StopNotifier that a stop signal has been received.
+//
+// NotifyStop은 2번 이상 호출돼도 예측한 대로 작동합니다.
 func (sn *StopNotifier) NotifyStop() {
 	sn.mu.Lock()
 	defer sn.mu.Unlock()
 
+	// The current channel is changed to closedChan because a closed channel does not block the flow.
+	// The reason for assigning closedChan instead of calling the close function is to consider the possibility of multiple calls.
+	// Closing an already closed channel causes a panic, so we need to check if the channel is already closed.
+	// Since no one is sending data to the channel at this time, this would result in indefinite blocking.
 	sn.ch = closedChan
 }
 
+// Done returns the channel that will be closed when a stop signal is received.
 func (sn *StopNotifier) Done() chan struct{} {
 	return sn.ch
 }
