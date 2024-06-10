eccffec6b3b7ec4427fd6be7fb97d7d0af3dfb73
orca <lsjtop10@gmail.com>
2024-06-10 14:11:16 +0900

    Refactor: clean channel mux

--- internal/job/channel_mux.go
+++ internal/job/channel_mux.go
@@ -1,13 +1,8 @@
 package job
 
-// ChannelMux copies the data received from a single input channel to multiple output channels.
-// It does not accept new inputs until all output channels have received the data.
-// Before using ChannelMux, you MUST execute the Execute() function.
 type ChannelMux struct {
 	in  DataChan
 	out []DataChan
-
-	stop chan struct{}
 }
 
 func (fo *ChannelMux) SetInput(in DataChan) {
@@ -28,18 +23,13 @@ func (fo *ChannelMux) Execute() {
 			}
 		}()
 
-		select {
-		case <-fo.stop:
+		data, ok := <-fo.in
+		if !ok {
 			return
-		case data, ok := <-fo.in:
-			if !ok {
-				close(fo.stop)
-				return
-			}
+		}
 
-			for _, ch := range fo.out {
-				ch <- data
-			}
+		for _, ch := range fo.out {
+			ch <- data
 		}
 
 	}()
