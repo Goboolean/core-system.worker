c104b427718364b27bfc299cfdd2af77e1200377
orca <lsjtop10@gmail.com>
2024-06-11 11:30:55 +0900

    Chore: move test tata to /test

--- internal/pipeline/builder_test.go
+++ internal/pipeline/builder_test.go
@@ -11,24 +11,30 @@ import (
 func TestBuilder(t *testing.T) {
 	t.Run("yml에 가상의 normal pipeline 시나리오가 주어졌을 때, 파이프라인을 적절히 빌드해야 한다.", func(t *testing.T) {
 		//arrange
-		cfg, err := configuration.ImportAppConfigFromFile("./testdata/normal.test.yml")
+		cfg, err := configuration.ImportAppConfigFromFile("../../test/pipeline_builder_testdata/normal.test.yml")
 		if err != nil {
 			t.Error(err)
 		}
 
+		//act
 		p, err := pipeline.Build(*cfg)
+
+		//assert
 		assert.NoError(t, err)
 		_, ok := p.(*pipeline.Normal)
 		assert.True(t, ok)
 	})
 	t.Run("yml에 가상의 without normal pipeline 시나리오가 주어졌을 때, 파이프라인을 적절히 빌드해야 한다.", func(t *testing.T) {
 		//arrange
-		cfg, err := configuration.ImportAppConfigFromFile("./testdata/without_model.test.yml")
+		cfg, err := configuration.ImportAppConfigFromFile("../../testdata/without_model.test.yml")
 		if err != nil {
 			t.Error(err)
 		}
 
+		//act
 		p, err := pipeline.Build(*cfg)
+
+		//assert
 		assert.NoError(t, err)
 		_, ok := p.(*pipeline.WithoutModel)
 		assert.True(t, ok)
