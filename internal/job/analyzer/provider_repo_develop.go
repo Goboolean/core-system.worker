015436dbc16ba2c1af56b5dc82bf6db9233d2b9b
orca <35363038+lsjtop10@users.noreply.github.com>
2024-06-12 15:17:47 +0900

    Test: add pipeline builder test and fix pipeline (#106)
    * Test: Add test for pipeline builder
    
    * Fix: change string() to fmt.Sprint
    
    * Refactor: inline extractAdapterSpec()
    
    - Removed abstraction to allow for more flexibility and clarity in adapter selection logic.
    
    * Fix: change builders to inject fake transmitter instead of Dummy
    
    * Feat: register provider for test to provider repository
    
    * Test: Complete to implement TestBuilder
    
    * Fix typo
    
    * Chore: Add testData
    
    * Chore: add product type stock stub
    
    * Chore: replace chan any to job.DataChan in Fake transmitter
    
    * Chore: move test tata to /test

--- internal/job/analyzer/provider_repo_develop.go
+++ internal/job/analyzer/provider_repo_develop.go
@@ -5,7 +5,10 @@ package analyzer
 import "github.com/Goboolean/core-system.worker/internal/job"
 
 var providerRepo = map[Spec]jobProvider{
-	{ID: "stub", InputType: "candlestick"}: func(p *job.UserParams) (Analyzer, error) {
+	{ID: "stub", InputType: "stub"}: func(p *job.UserParams) (Analyzer, error) {
+		return NewStub(p)
+	},
+	{ID: "stub", InputType: "stockStub"}: func(p *job.UserParams) (Analyzer, error) {
 		return NewStub(p)
 	},
 }
