ff3782cbdeadbcf111b32f26296e6ba616eb0271
orca <lsjtop10@gmail.com>
2024-06-14 13:53:39 +0900

    Feat: change fetching infra interface

--- internal/job/fetcher/interface.go
+++ internal/job/fetcher/interface.go
@@ -1,9 +1,12 @@
 package fetcher
 
+//go:generate mockgen -destination=fetching_infra_mock.go -package=fetcher --build_flags=--mod=mod . TradeRepository,FetchingSession,TradeStream
+
 // fetcher는 pipeline의 trade data fetch 단계를 수행할 수 있는 Job을 구현합니다.
 // 모든 fetch job은 Fetcher 인터페이스를 구현해야 합니다.
 
 import (
+	"context"
 	"time"
 
 	"github.com/Goboolean/core-system.worker/internal/job"
@@ -19,17 +22,15 @@ type Fetcher interface {
 
 // FetchingSession is an interface that represents a fetching session.
 type FetchingSession interface {
-	// TryNext returns true if there is another session to try.
-	TryNext() bool
+	// Next advances the session to the next item.
+	// It returns true if there is a next item, false otherwise.
+	//
+	// Next must be called initially to retrieve the first item.
+	Next() bool
 
-	// Next returns the next fetching session and an error, if any.
-	Next() (FetchingSession, error)
-
-	// DecodedValue decodes the value into the provided variable.
-	DecodedValue(v any) error
-
-	// Close closes the fetching session.
-	Close() error
+	// Value returns the current item in the fetching session.
+	// It returns the item and an error if there was an error retrieving the item.
+	Value(ctx context.Context) (any, error)
 }
 
 // TradeRepository is an interface that represents a trade repository.
@@ -41,7 +42,10 @@ type TradeRepository interface {
 	SetRangeByTime(from time.Time, to time.Time)
 
 	// Session returns a fetching session.
-	Session() FetchingSession
+	Session() (FetchingSession, error)
+
+	// Close closes the connection
+	Close() error
 }
 
 // TradeStream is an interface that represents a trade stream.
@@ -50,5 +54,8 @@ type TradeStream interface {
 	SelectProduct(ID string, timeFrame string, productType string)
 
 	// Session returns a fetching session.
-	Session() FetchingSession
+	Session() (FetchingSession, error)
+
+	// Close closes the connection.
+	Close() error
 }
