99d72f4f9b4111fd40d9b12a5452146ac89b7c13
orca <lsjtop10@gmail.com>
2024-06-14 18:09:06 +0900

    Feat: implement channel demux

---
+++ internal/util/channel_demux.go
@@ -0,0 +1,44 @@
+package util
+
+import "sync"
+
+type ChannelDeMux[T any] struct {
+	in  []chan T
+	out chan T
+}
+
+func NewChannelDeMux[T any]() *ChannelDeMux[T] {
+	return &ChannelDeMux[T]{
+		in:  make([]chan T, 0),
+		out: make(chan T),
+	}
+}
+
+func (dm *ChannelDeMux[T]) SetInput(in chan T) {
+	dm.in = append(dm.in, in)
+}
+
+func (dm *ChannelDeMux[T]) Output() chan T {
+	return dm.out
+}
+
+func (dm *ChannelDeMux[T]) Execute() {
+	wg := &sync.WaitGroup{}
+	for _, e := range dm.in {
+		wg.Add(1)
+		go func(ch chan T) {
+			defer close(ch)
+			defer wg.Done()
+
+			for v := range ch {
+				dm.out <- v
+			}
+
+		}(e)
+	}
+
+	go func() {
+		wg.Wait()
+		close(dm.out)
+	}()
+}
