e7e109f8ffbe4b56202f0d8b195d7edf23fc2e84
orca <35363038+lsjtop10@users.noreply.github.com>
2024-06-17 10:40:25 +0900

    Chore: remove DI config because adapter is not implemented (#117)

--- internal/job/fetcher/wrie_setup.go
+++ internal/job/fetcher/wrie_setup.go
@@ -11,37 +11,3 @@ import (
 )
 
 type mongoConfig resolver.ConfigMap
-
-func provideMongoConfig() mongoConfig {
-	//임시
-	return mongoConfig(resolver.ConfigMap{})
-}
-
-func provideMongo(c mongoConfig) (*mongo.StockClientImpl, error) {
-	in := resolver.ConfigMap(c)
-	return mongo.NewStockClientImpl(&in)
-}
-
-func initializeRealtimeStock(p *job.UserParams) (Fetcher, error) {
-	wire.Build(
-		provideMongoConfig,
-		provideMongo,
-		NewRealtimeStock,
-		wire.Bind(new(Fetcher), new(*RealtimeStock)),
-		wire.Bind(new(mongo.StockClient), new(*mongo.StockClientImpl)),
-	)
-
-	return nil, nil
-}
-
-func initializePastStock(p *job.UserParams) (Fetcher, error) {
-	wire.Build(
-		provideMongoConfig,
-		provideMongo,
-		NewPastStock,
-		wire.Bind(new(Fetcher), new(*PastStock)),
-		wire.Bind(new(mongo.StockClient), new(*mongo.StockClientImpl)),
-	)
-
-	return nil, nil
-}
