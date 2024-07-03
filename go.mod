e60775a049c2a2cd50060ff6ca192761138d1ebf
orca <lsjtop10@gmail.com>
2024-07-03 10:32:39 +0900

    Feat: implement influxdb client for dispatch
    - change dispatcher interface

--- go.mod
+++ go.mod
@@ -6,17 +6,26 @@ toolchain go1.22.2
 
 require (
 	github.com/Goboolean/common v0.0.0-20231130153141-cb54596b217d
+	github.com/Goboolean/fetch-system.IaC v0.0.0-20240702021750-255409631827
 	github.com/cenkalti/backoff v2.2.1+incompatible
 	github.com/google/wire v0.6.0
+	github.com/influxdata/influxdb-client-go v1.4.0
+	github.com/influxdata/influxdb-client-go/v2 v2.13.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/mock v0.4.0
+	golang.org/x/sync v0.7.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/oapi-codegen/runtime v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
---
+++ internal/infrastructure/influx/dispatchers.go
@@ -0,0 +1,143 @@
+package influx
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/Goboolean/core-system.worker/internal/model"
+	"github.com/Goboolean/fetch-system.IaC/pkg/influx/mapper"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
+	"github.com/influxdata/influxdb-client-go/v2/api/write" // Change the import statement to use the correct version of the Point type
+)
+
+var ErrBucketNotExist = errors.New("create dispatcher: influxdb bucket does not exists")
+
+type Opts struct {
+	Url        string
+	Token      string
+	Org        string
+	BucketName string
+}
+
+type OrderEventDispatcher struct {
+	// Dispatch dispatches the given order event.
+	client influxdb2.Client
+	writer api.WriteAPI
+}
+
+func NewOrderEventDispatcher(o *Opts) (*OrderEventDispatcher, error) {
+
+	if o.Url == "" {
+		return nil, fmt.Errorf("create influx db client: Required field Url is blank")
+	}
+
+	if o.Token == "" {
+		return nil, fmt.Errorf("create influx db client: Required field Token is blank")
+	}
+
+	if o.Org == "" {
+		return nil, fmt.Errorf("create influx db client: Required field Url is blank")
+	}
+
+	if o.BucketName == "" {
+		return nil, fmt.Errorf("create influx db client: Required field TradeBucketName is blank")
+	}
+
+	client := influxdb2.NewClient(o.Url, o.Token)
+
+	if !bucketExists(client, o.BucketName) {
+		return nil, ErrBucketNotExist
+	}
+
+	instance := &OrderEventDispatcher{
+		client: client,
+		writer: client.WriteAPI(o.Org, o.BucketName),
+	}
+
+	return instance, nil
+}
+
+func (d *OrderEventDispatcher) Dispatch(taskID string, event *model.OrderEvent) {
+	d.writer.WritePoint(write.NewPoint(
+		taskID,
+		map[string]string{},
+		map[string]interface{}{
+			"productID":         event.ProductID,
+			"proportionPercent": event.Command.ProportionPercent,
+			"action":            event.Command.Action.String(),
+			"task":              event.Task.String(),
+		},
+		event.CreatedAt,
+	))
+
+}
+func (d *OrderEventDispatcher) Close() error {
+	d.writer.Flush()
+	d.client.Close()
+	return nil
+}
+
+// Close closes the dispatcher.
+
+type AnnotationDispatcher struct {
+	client influxdb2.Client
+	writer api.WriteAPI
+}
+
+func NewAnnotationDispatcher(o *Opts) (*AnnotationDispatcher, error) {
+
+	if o.Url == "" {
+		return nil, fmt.Errorf("create influx db client: Required field Url is blank")
+	}
+
+	if o.Token == "" {
+		return nil, fmt.Errorf("create influx db client: Required field Token is blank")
+	}
+
+	if o.Org == "" {
+		return nil, fmt.Errorf("create influx db client: Required field Url is blank")
+	}
+
+	if o.BucketName == "" {
+		return nil, fmt.Errorf("create influx db client: Required field TradeBucketName is blank")
+	}
+
+	client := influxdb2.NewClient(o.Url, o.Token)
+	if !bucketExists(client, o.BucketName) {
+		return nil, ErrBucketNotExist
+	}
+
+	instance := &AnnotationDispatcher{
+		client: client,
+		writer: client.WriteAPI(o.Org, o.BucketName),
+	}
+
+	return instance, nil
+}
+
+// Dispatch dispatches the given data.
+func (d *AnnotationDispatcher) Dispatch(taskID string, data any, createdAt time.Time) {
+	influxDataField, _ := mapper.StructToPoint(data)
+	d.writer.WritePoint(write.NewPoint(
+		taskID,
+		map[string]string{},
+		influxDataField,
+		createdAt,
+	))
+}
+
+// Close closes the dispatcher.
+func (d *AnnotationDispatcher) Close() error {
+	d.writer.Flush()
+	d.client.Close()
+	return nil
+}
+
+func bucketExists(c influxdb2.Client, bucket string) bool {
+	bucketApi := c.BucketsAPI()
+	_, err := bucketApi.FindBucketByName(context.Background(), bucket)
+	return err == nil
+}
