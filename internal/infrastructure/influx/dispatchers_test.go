212c961014766ca8316d6dd62c773968217351e1
orca <lsjtop10@gmail.com>
2024-07-08 08:47:53 +0900

    Refactor: move configuration and reusable function to main.go

--- internal/infrastructure/influx/dispatchers_test.go
+++ internal/infrastructure/influx/dispatchers_test.go
@@ -3,53 +3,13 @@ package influx_test
 import (
 	"context"
 	"fmt"
-	"os"
 	"testing"
 	"time"
 
 	"github.com/Goboolean/core-system.worker/internal/infrastructure/influx"
 	"github.com/Goboolean/core-system.worker/internal/model"
-	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/stretchr/testify/assert"
 )
-
-var (
-	url    = os.Getenv("INFLUXDB_URL")
-	bucket = os.Getenv("INFLUXDB_BUCKET")
-	token  = os.Getenv("INFLUXDB_TOKEN")
-	org    = os.Getenv("INFLUXDB_ORG")
-)
-
-var rawInfluxDBClient influxdb2.Client
-
-var (
-	productID = "stock.aapl.usa"
-	taskID    = "2024-07-05-test"
-)
-
-func TestMain(m *testing.M) {
-	rawInfluxDBClient = influxdb2.NewClient(url, token)
-	m.Run()
-	rawInfluxDBClient.Close()
-}
-
-func RecreateBucket(client influxdb2.Client, orgName, bucketName string) error {
-
-	org, err := client.OrganizationsAPI().FindOrganizationByName(context.Background(), orgName)
-	if err != nil {
-		return err
-	}
-
-	bucket, err := client.BucketsAPI().FindBucketByName(context.Background(), bucketName)
-	if err != nil {
-		return nil
-	}
-
-	client.BucketsAPI().DeleteBucket(context.Background(), bucket)
-	_, err = client.BucketsAPI().CreateBucketWithName(context.Background(), org, bucketName)
-
-	return err
-}
 
 func TestOrderEventDispatcher(t *testing.T) {
 	t.Run("발송한 order event의 개수와 bucket에 있는 order event의 개수가 같아야 한다.", func(t *testing.T) {
