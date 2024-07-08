85b696a87da327c673e60c0c0d95f27fc3c6e070
orca <lsjtop10@gmail.com>
2024-07-08 15:41:24 +0900

    Chore: add integration test for without model pipeline

---
+++ test/integration/main_test.go
@@ -0,0 +1,45 @@
+package integration
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+)
+
+var rawInfluxClient influxdb2.Client
+
+var (
+	influxDBUrl   = os.Getenv("INFLUXDB_HOST")
+	influxDBOrg   = os.Getenv("INFLUXDB_ORG")
+	influxDBToken = os.Getenv("INFLUXDB_TOKEN")
+
+	tradeBucket      = os.Getenv("INFLUXDB_TRADE_BUCKET")
+	orderBucket      = os.Getenv("INFLUXDB_ORDER_EVENT_BUCKET")
+	annotationBucket = os.Getenv("INFLUXDB_ORDER_EVENT_BUCKET")
+)
+
+func RecreateBucket(client influxdb2.Client, orgName, bucketName string) error {
+
+	org, err := client.OrganizationsAPI().FindOrganizationByName(context.Background(), orgName)
+	if err != nil {
+		return err
+	}
+
+	bucket, err := client.BucketsAPI().FindBucketByName(context.Background(), bucketName)
+	if err != nil {
+		return nil
+	}
+
+	client.BucketsAPI().DeleteBucket(context.Background(), bucket)
+	_, err = client.BucketsAPI().CreateBucketWithName(context.Background(), org, bucketName)
+
+	return err
+}
+
+func TestMain(m *testing.M) {
+	rawInfluxClient = influxdb2.NewClient(influxDBUrl, influxDBToken)
+	m.Run()
+	rawInfluxClient.Close()
+}
