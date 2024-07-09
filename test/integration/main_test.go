445d8f952c0656bf8597aff5e9172477eef7f789
orca <35363038+lsjtop10@users.noreply.github.com>
2024-07-09 17:29:59 +0900

    Write integration test (#190)
    * Chore: add integration test for without model pipeline
    
    * Add integration test for normal pipeline
    
    * Update test/integration/normal.test.yml
    
    * Chore: add -v option to test
    
    * Try fixing build failure
    
    - fix wrong receiver
    - remove fetching infra mock
    
    * Try resolving unused
    
    * Feat: add ping test
    
    * Fix: fix wrong env variable
    
    * Feat: Add error check
    
    * Fix Product Type
    
    * Revert "Fix Product Type"
    
    This reverts commit db5635095e6e7d8831906c039cf1e4b3e2f2c094.
    
    * Fix wrong annotation bucket name
    
    * Chore: add errcheck
    
    * Revert "Chore: add -v option to test"
    
    This reverts commit 5c454822ef548206d0ff7a81cb3c9d8dc972182b.
    
    * Revert "Try resolving unused"
    
    This reverts commit ab357c30ceb586c7494dffdc3ef786b029599268.

---
+++ test/integration/main_test.go
@@ -0,0 +1,53 @@
+package integration
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+var rawInfluxClient influxdb2.Client
+
+var (
+	influxDBUrl   = os.Getenv("INFLUXDB_URL")
+	influxDBOrg   = os.Getenv("INFLUXDB_ORG")
+	influxDBToken = os.Getenv("INFLUXDB_TOKEN")
+
+	tradeBucket      = os.Getenv("INFLUXDB_TRADE_BUCKET")
+	orderBucket      = os.Getenv("INFLUXDB_ORDER_EVENT_BUCKET")
+	annotationBucket = os.Getenv("INFLUXDB_ANNOTATION_EVENT_BUCKET")
+)
+
+func RecreateBucket(client influxdb2.Client, orgName, bucketName string) error {
+	org, err := client.OrganizationsAPI().FindOrganizationByName(context.Background(), orgName)
+	if err != nil {
+		return err
+	}
+
+	bucket, err := client.BucketsAPI().FindBucketByName(context.Background(), bucketName)
+	if err != nil {
+		return nil
+	}
+
+	if err := client.BucketsAPI().DeleteBucket(context.Background(), bucket); err != nil {
+		return err
+	}
+	_, err = client.BucketsAPI().CreateBucketWithName(context.Background(), org, bucketName)
+
+	return err
+}
+
+func TestPing(t *testing.T) {
+	ok, err := rawInfluxClient.Ping(context.Background())
+	assert.True(t, ok)
+	assert.NoError(t, err)
+}
+
+func TestMain(m *testing.M) {
+	rawInfluxClient = influxdb2.NewClient(influxDBUrl, influxDBToken)
+	m.Run()
+	rawInfluxClient.Close()
+}
---
+++ test/integration/without_model_test.go
@@ -0,0 +1,93 @@
+package integration
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/Goboolean/core-system.worker/configuration"
+	"github.com/Goboolean/core-system.worker/internal/pipeline"
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPipelineWithoutModel(t *testing.T) {
+	t.Run("backTesting useCase에서 influxDB에 데이터가 N개 저장됐을 때,"+
+		"annotation과 orderEvent에도 N개가 저장돼야 한다.", func(t *testing.T) {
+		//arrange
+		if err := RecreateBucket(rawInfluxClient, influxDBOrg, tradeBucket); err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		if err := RecreateBucket(rawInfluxClient, influxDBOrg, orderBucket); err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		if err := RecreateBucket(rawInfluxClient, influxDBOrg, annotationBucket); err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+
+		startTime := time.Unix(1720396800, 0)
+		num := 390
+		writer := rawInfluxClient.WriteAPIBlocking(influxDBOrg, tradeBucket)
+		for i := 0; i < num; i++ {
+			err := writer.WritePoint(context.Background(),
+				write.NewPoint(
+					"stock.aapl.usa.1m",
+					map[string]string{},
+					map[string]interface{}{
+						"open":   float64(i),
+						"close":  float64(2.0),
+						"high":   float64(3.0),
+						"low":    float64(4.0),
+						"volume": float64(4),
+					},
+					startTime.Add(time.Duration(i)*time.Minute),
+				))
+			if err != nil {
+				t.Error(err)
+				t.FailNow()
+			}
+		}
+
+		//act
+		config, err := configuration.ImportAppConfigFromFile("./without_model.test.yml")
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+
+		p, err := pipeline.Build(*config)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+
+		ctx := context.Background()
+		err = p.Run(ctx)
+
+		//assert
+		assert.NoError(t, err)
+
+		q, err := rawInfluxClient.QueryAPI(influxDBOrg).
+			Query(context.Background(),
+				fmt.Sprintf(`from(bucket: "%s")
+			|> range(start:0)
+			|> filter(fn: (r) => r["_measurement"] == "%s")
+			|> count())`, orderBucket, config.TaskID))
+
+		assert.NoError(t, err)
+		assert.Equal(t, num, q.Record().ValueByKey(q.TableMetadata().Column(0).Name()))
+
+		q, err = rawInfluxClient.QueryAPI(influxDBOrg).
+			Query(context.Background(),
+				fmt.Sprintf(`from(bucket: "%s")
+		|> range(start:0)
+		|> filter(fn: (r) => r["_measurement"] == "%s")
+		|> count())`, annotationBucket, config.TaskID))
+		assert.NoError(t, err)
+		assert.Equal(t, num, q.Record().ValueByKey(q.TableMetadata().Column(0).Name()))
+	})
+}
