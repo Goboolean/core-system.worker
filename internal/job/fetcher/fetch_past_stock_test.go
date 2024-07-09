a1081ce41cfd0834bddabb0897c10b10cb5fa85a
orca <lsjtop10@gmail.com>
2024-07-09 22:39:09 +0900

    Try fix test to store smaller data

--- internal/job/fetcher/fetch_past_stock_test.go
+++ internal/job/fetcher/fetch_past_stock_test.go
@@ -2,11 +2,19 @@ package fetcher_test
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/Goboolean/core-system.worker/internal/job"
+	"github.com/Goboolean/core-system.worker/internal/job/fetcher"
+	"github.com/Goboolean/core-system.worker/internal/model"
 	"github.com/Goboolean/fetch-system.IaC/pkg/influx"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
+	"github.com/stretchr/testify/assert"
 )
 
 var opts = influx.Opts{
@@ -48,137 +56,137 @@ func RecreateBucket(client influxdb2.Client, orgName, bucketName string) error {
 	return err
 }
 
-// func TestPing(t *testing.T) {
-// 	ok, err := rawInfluxClient.Ping(context.Background())
-// 	assert.True(t, ok)
-// 	assert.NoError(t, err)
-// }
+func TestPing(t *testing.T) {
+	ok, err := rawInfluxClient.Ping(context.Background())
+	assert.True(t, ok)
+	assert.NoError(t, err)
+}
 
 func TestPastStock(t *testing.T) {
-	// 	t.Run("저장된 데이터가 없을 때, 0개의 데이터를 가져와야 한다.", func(t *testing.T) {
-	// 		//arrange
-	// 		if err := RecreateBucket(rawInfluxClient, opts.Org, opts.TradeBucketName); err != nil {
-	// 			t.Error(err)
-	// 			t.FailNow()
-	// 		}
-	// 		start := time.Now()
+	t.Run("저장된 데이터가 없을 때, 0개의 데이터를 가져와야 한다.", func(t *testing.T) {
+		//arrange
+		if err := RecreateBucket(rawInfluxClient, opts.Org, opts.TradeBucketName); err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		start := time.Now()
 
-	// 		query, err := influx.NewDB(&opts)
-	// 		if err != nil {
-	// 			t.Error(err)
-	// 			t.FailNow()
-	// 		}
+		query, err := influx.NewDB(&opts)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
 
-	// 		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second))
-	// 		defer cancel()
-	// 		err = query.Ping(ctx)
-	// 		if err != nil {
-	// 			t.Error(err)
-	// 			t.FailNow()
-	// 		}
+		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second))
+		defer cancel()
+		err = query.Ping(ctx)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
 
-	// 		cursor, err := fetcher.NewStockTradeCursor(query)
-	// 		if err != nil {
-	// 			t.Error(err)
-	// 			t.FailNow()
-	// 		}
+		cursor, err := fetcher.NewStockTradeCursor(query)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
 
-	// 		fetchJob, err := fetcher.NewPastStock(cursor, &job.UserParams{
-	// 			job.ProductID: testStockID,
-	// 			job.StartDate: fmt.Sprint(start.Unix()),
-	// 			job.EndDate:   fmt.Sprint(start.Add(time.Minute).Unix()),
-	// 		})
-	// 		if err != nil {
-	// 			t.Error(err)
-	// 			t.FailNow()
-	// 		}
+		fetchJob, err := fetcher.NewPastStock(cursor, &job.UserParams{
+			job.ProductID: testStockID,
+			job.StartDate: fmt.Sprint(start.Unix()),
+			job.EndDate:   fmt.Sprint(start.Add(time.Minute).Unix()),
+		})
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
 
-	// 		out := make([]model.Packet, 0)
-	// 		go func() {
-	// 			for v := range fetchJob.Output() {
-	// 				out = append(out, v)
-	// 			}
-	// 		}()
+		out := make([]model.Packet, 0)
+		go func() {
+			for v := range fetchJob.Output() {
+				out = append(out, v)
+			}
+		}()
 
-	// 		err = fetchJob.Execute()
+		err = fetchJob.Execute()
 
-	// 		assert.NoError(t, err)
-	// 		assert.Len(t, out, 0)
-	// 	})
+		assert.NoError(t, err)
+		assert.Len(t, out, 0)
+	})
 
-	// t.Run("데이터가 저장된 만큼, 데이터를 가져와야 한다.", func(t *testing.T) {
+	t.Run("데이터가 저장된 만큼, 데이터를 가져와야 한다.", func(t *testing.T) {
 
-	// 	if err := RecreateBucket(rawInfluxClient, opts.Org, opts.TradeBucketName); err != nil {
-	// 		t.Error(err)
-	// 		t.FailNow()
-	// 	}
-	// 	writer := rawInfluxClient.WriteAPIBlocking(opts.Org, opts.TradeBucketName)
-	// 	storeNum := int(rand.Int31n(100))
-	// 	storeInterval := time.Minute
-	// 	start := time.Now().Add(-time.Duration(storeNum) * storeInterval)
-	// 	for i := 0; i < storeNum; i++ {
-	// 		err := writer.WritePoint(
-	// 			context.Background(),
-	// 			write.NewPoint(
-	// 				fmt.Sprintf("%s.%s", testStockID, testTimeFrame),
-	// 				map[string]string{},
-	// 				map[string]interface{}{
-	// 					"open":   float64(i),
-	// 					"close":  float64(2.0),
-	// 					"high":   float64(3.0),
-	// 					"low":    float64(4.0),
-	// 					"volume": float64(4),
-	// 				},
-	// 				start.Add(time.Duration(i)*storeInterval),
-	// 			),
-	// 		)
+		if err := RecreateBucket(rawInfluxClient, opts.Org, opts.TradeBucketName); err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		writer := rawInfluxClient.WriteAPIBlocking(opts.Org, opts.TradeBucketName)
+		storeNum := int(rand.Int31n(20))
+		storeInterval := time.Minute
+		start := time.Now().Add(-time.Duration(storeNum) * storeInterval)
+		for i := 0; i < storeNum; i++ {
+			err := writer.WritePoint(
+				context.Background(),
+				write.NewPoint(
+					fmt.Sprintf("%s.%s", testStockID, testTimeFrame),
+					map[string]string{},
+					map[string]interface{}{
+						"open":   float64(i),
+						"close":  float64(2.0),
+						"high":   float64(3.0),
+						"low":    float64(4.0),
+						"volume": float64(4),
+					},
+					start.Add(time.Duration(i)*storeInterval),
+				),
+			)
 
-	// 		if err != nil {
-	// 			t.Error(err)
-	// 			t.FailNow()
-	// 		}
-	// 	}
+			if err != nil {
+				t.Error(err)
+				t.FailNow()
+			}
+		}
 
-	// 	query, err := influx.NewDB(&opts)
-	// 	if err != nil {
-	// 		t.Error(err)
-	// 		t.FailNow()
-	// 	}
-	// 	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second))
-	// 	defer cancel()
-	// 	err = query.Ping(ctx)
-	// 	if err != nil {
-	// 		t.Error(err)
-	// 		t.FailNow()
-	// 	}
+		query, err := influx.NewDB(&opts)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second))
+		defer cancel()
+		err = query.Ping(ctx)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
 
-	// 	cursor, err := fetcher.NewStockTradeCursor(query)
-	// 	if err != nil {
-	// 		t.Error(err)
-	// 		t.FailNow()
-	// 	}
+		cursor, err := fetcher.NewStockTradeCursor(query)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
 
-	// 	fetchJob, err := fetcher.NewPastStock(cursor, &job.UserParams{
-	// 		job.ProductID: testStockID,
-	// 		job.StartDate: fmt.Sprint(start.Unix()),
-	// 		job.EndDate:   fmt.Sprint(start.Add(time.Duration(storeNum) * storeInterval).Unix()),
-	// 	})
-	// 	if err != nil {
-	// 		t.Error(err)
-	// 		t.FailNow()
-	// 	}
+		fetchJob, err := fetcher.NewPastStock(cursor, &job.UserParams{
+			job.ProductID: testStockID,
+			job.StartDate: fmt.Sprint(start.Unix()),
+			job.EndDate:   fmt.Sprint(start.Add(time.Duration(storeNum) * storeInterval).Unix()),
+		})
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
 
-	// 	out := make([]model.Packet, 0)
-	// 	go func() {
-	// 		for v := range fetchJob.Output() {
-	// 			out = append(out, v)
-	// 		}
-	// 	}()
+		out := make([]model.Packet, 0)
+		go func() {
+			for v := range fetchJob.Output() {
+				out = append(out, v)
+			}
+		}()
 
-	// 	err = fetchJob.Execute()
+		err = fetchJob.Execute()
 
-	// 	assert.NoError(t, err)
-	// 	assert.Len(t, out, storeNum)
+		assert.NoError(t, err)
+		assert.Len(t, out, storeNum)
 
-	// })
+	})
 }
