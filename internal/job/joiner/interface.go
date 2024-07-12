7831cef0da416e3607cd6ec20a9df666dd6e10e6
orca <lsjtop10@gmail.com>
2024-07-12 16:59:21 +0900

    Doc: add comment for joiner interface

--- internal/job/joiner/interface.go
+++ internal/job/joiner/interface.go
@@ -4,10 +4,17 @@ import (
 	"github.com/Goboolean/core-system.worker/internal/job"
 )
 
+// Joiner is an interface that job implementations for the join stage of the pipeline
+// Joiner represents a job interface that performs joining operations.
 type Joiner interface {
 	job.Common
 
+	// SetRefInput sets the reference input data channel for the joiner.
 	SetRefInput(job.DataChan)
+
+	// SetModelInput sets the model input data channel for the joiner.
 	SetModelInput(job.DataChan)
+
+	// Output returns the output data channel for the joiner.
 	Output() job.DataChan
 }
