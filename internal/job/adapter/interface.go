ff59d6a4066afcec55d2c3f17570a1f091ca0538
orca <lsjtop10@gmail.com>
2024-07-15 09:06:30 +0900

    Doc: add comment for interface

--- internal/job/adapter/interface.go
+++ internal/job/adapter/interface.go
@@ -4,10 +4,14 @@ import (
 	"github.com/Goboolean/core-system.worker/internal/job"
 )
 
+// Adapter is an interface that Job implementations for the adapt stage of the pipeline
 type Adapter interface {
 	job.Common
 
+	// SetInput sets the input data channel for the analyzer job.
 	SetInput(job.DataChan)
+
+	// Output returns the output data channel for the analyzer job.
 	Output() job.DataChan
 }
 
