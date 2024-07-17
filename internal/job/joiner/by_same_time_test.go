59d1dc8e4bbb0d3a66ee8fe9be57be5237d23dca
orca <lsjtop10@gmail.com>
2024-07-17 16:23:28 +0900

    Refactor: introduce test suite to joiner

--- internal/job/joiner/by_same_time_test.go
+++ internal/job/joiner/by_same_time_test.go
@@ -9,105 +9,107 @@ import (
 	"github.com/Goboolean/core-system.worker/internal/job/joiner"
 	"github.com/Goboolean/core-system.worker/internal/model"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestMain(m *testing.M) {
 	m.Run()
 }
 
-func TestJoinByTime(t *testing.T) {
-	t.Run("Sequence가 같은 두 데이터가 주어졌을 때, 이 두 데이터를 join해서 출력해야 한다.", func(t *testing.T) {
-		for i := 0; i < 100; i++ {
-			start := time.Now()
-			//arrange
-			referenceInput := []model.Packet{
-				{
-					Time: start,
-					Data: 1,
-				},
-				{
-					Time: start.Add(time.Duration(1) * time.Second),
-					Data: 1,
-				},
-				{
-					Time: start.Add(time.Duration(2) * time.Second),
-					Data: 1,
-				},
-			}
+type ByTimeTestSuite struct {
+	suite.Suite
+}
 
-			modelInput := []model.Packet{
-
-				{
-					Time: start.Add(time.Duration(1) * time.Second),
-					Data: 2,
-				},
-				{
-					Time: start.Add(time.Duration(2) * time.Second),
-					Data: 2,
-				},
-			}
-
-			exp := []model.Packet{
-				{
-					Time: start.Add(time.Duration(1) * time.Second),
-					Data: &model.Pair{
-						RefData:   1,
-						ModelData: 2,
-					},
-				},
-				{
-					Time: start.Add(time.Duration(2) * time.Second),
-					Data: &model.Pair{
-						RefData:   1,
-						ModelData: 2,
-					},
-				},
-			}
-
-			referenceInputChan := make(job.DataChan)
-			modelInputChan := make(job.DataChan)
-			// Insert data into the input channel using a goroutines to insert randomly
-			go func() {
-				defer close(referenceInputChan)
-				for _, e := range referenceInput {
-					referenceInputChan <- e
-				}
-			}()
-			go func() {
-				defer close(modelInputChan)
-				for _, e := range modelInput {
-					modelInputChan <- e
-				}
-			}()
-
-			joinJob, err := joiner.NewByTime(&job.UserParams{})
-			if err != nil {
-				t.Error(err)
-				return
-			}
-
-			joinJob.SetRefInput(referenceInputChan)
-			joinJob.SetModelInput(modelInputChan)
-
-			//act
-			res := make([]model.Packet, 0)
-
-			wg := &sync.WaitGroup{}
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				for v := range joinJob.Output() {
-					res = append(res, v)
-				}
-			}()
-
-			err = joinJob.Execute()
-			wg.Wait()
-
-			//assert
-			assert.NoError(t, err)
-			assert.Equal(t, exp, res)
+func (suite *ByTimeTestSuite) TestByTime_ShouldPackDataTogether_WhenDataFromReferenceInputAndFromModelInputHaveSameTime() {
+	for i := 0; i < 100; i++ {
+		start := time.Now()
+		//arrange
+		referenceInput := []model.Packet{
+			{
+				Time: start,
+				Data: 1,
+			},
+			{
+				Time: start.Add(time.Duration(1) * time.Second),
+				Data: 1,
+			},
+			{
+				Time: start.Add(time.Duration(2) * time.Second),
+				Data: 1,
+			},
 		}
-	})
 
+		modelInput := []model.Packet{
+
+			{
+				Time: start.Add(time.Duration(1) * time.Second),
+				Data: 2,
+			},
+			{
+				Time: start.Add(time.Duration(2) * time.Second),
+				Data: 2,
+			},
+		}
+
+		exp := []model.Packet{
+			{
+				Time: start.Add(time.Duration(1) * time.Second),
+				Data: &model.Pair{
+					RefData:   1,
+					ModelData: 2,
+				},
+			},
+			{
+				Time: start.Add(time.Duration(2) * time.Second),
+				Data: &model.Pair{
+					RefData:   1,
+					ModelData: 2,
+				},
+			},
+		}
+
+		referenceInputChan := make(job.DataChan)
+		modelInputChan := make(job.DataChan)
+		// Insert data into the input channel using a goroutines to insert randomly
+		go func() {
+			defer close(referenceInputChan)
+			for _, e := range referenceInput {
+				referenceInputChan <- e
+			}
+		}()
+		go func() {
+			defer close(modelInputChan)
+			for _, e := range modelInput {
+				modelInputChan <- e
+			}
+		}()
+
+		joinJob, err := joiner.NewByTime(&job.UserParams{})
+		if err != nil {
+			suite.T().Error(err)
+			return
+		}
+
+		joinJob.SetRefInput(referenceInputChan)
+		joinJob.SetModelInput(modelInputChan)
+
+		//act
+		res := make([]model.Packet, 0)
+
+		wg := &sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for v := range joinJob.Output() {
+				res = append(res, v)
+			}
+		}()
+
+		err = joinJob.Execute()
+		wg.Wait()
+
+		//assert
+		assert.NoError(suite.T(), err)
+		assert.Equal(suite.T(), exp, res)
+	}
 }
