b0db52ec58dd0e1f3b829ba7e30a936ff3d85ca6
orca <35363038+lsjtop10@users.noreply.github.com>
2024-07-18 10:38:31 +0900

    Refactor: refactor test of executer (#212)
    * Refactor: refactor test of executer
    
    * Add missing test entrypoint
    
    * Refactor: replace assert.XXX to suite.XXX
    
    * Refactor: simplify deadline check code

--- internal/job/executer/stub_test.go
+++ internal/job/executer/stub_test.go
@@ -1,61 +1,60 @@
 package executer_test
 
 import (
-	"testing"
 	"time"
 
 	"github.com/Goboolean/core-system.worker/internal/job"
 	"github.com/Goboolean/core-system.worker/internal/job/executer"
 	"github.com/Goboolean/core-system.worker/internal/model"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"golang.org/x/sync/errgroup"
 )
 
-func TestStub(t *testing.T) {
-	t.Run("stub에 N개의 데이터가 입력됐을 때 N개의 데이터를 출력해야 한다.", func(t *testing.T) {
-		//arrange
-		num := 100
-		inChan := make(job.DataChan, num)
-		start := time.Now()
-		for i := 0; i < num; i++ {
-			inChan <- model.Packet{
-				Time: start.Add(time.Duration(i) * time.Second),
-				Data: &model.StockAggregate{
-					OpenTime:   1716775499,
-					ClosedTime: 1716775499,
-					Open:       1.0,
-					Close:      2.0,
-					High:       3.0,
-					Low:        4.0,
-					Volume:     5.0,
-				},
-			}
+type StubTestSuite struct {
+	suite.Suite
+}
+
+func (suite *StubTestSuite) TestStub_ShouldReturnNDataItems_WhenNItemsAreStubbed() {
+	//arrange
+	num := 100
+	inChan := make(job.DataChan, num)
+	start := time.Now()
+	for i := 0; i < num; i++ {
+		inChan <- model.Packet{
+			Time: start.Add(time.Duration(i) * time.Second),
+			Data: &model.StockAggregate{
+				OpenTime:   1716775499,
+				ClosedTime: 1716775499,
+				Open:       1.0,
+				Close:      2.0,
+				High:       3.0,
+				Low:        4.0,
+				Volume:     5.0,
+			},
 		}
-		close(inChan)
-		stub, err := executer.NewStub(&job.UserParams{})
-		if err != nil {
-			t.Error(err)
-			return
+	}
+	close(inChan)
+
+	stub, err := executer.NewStub(&job.UserParams{})
+	suite.Require().NoError(err)
+
+	stub.SetInput(inChan)
+
+	//act
+	res := make([]model.Packet, 0, num)
+	g := errgroup.Group{}
+
+	g.Go(func() error {
+		for v := range stub.Output() {
+			res = append(res, v)
 		}
-		stub.SetInput(inChan)
-
-		//act
-
-		res := make([]model.Packet, 0, num)
-		g := errgroup.Group{}
-
-		g.Go(func() error {
-			for v := range stub.Output() {
-				res = append(res, v)
-			}
-			return nil
-		})
-
-		g.Go(stub.Execute)
-		err = g.Wait()
-
-		//assert
-		assert.NoError(t, err)
-		assert.Len(t, res, num)
+		return nil
 	})
+
+	g.Go(stub.Execute)
+	err = g.Wait()
+
+	//assert
+	suite.NoError(err)
+	suite.Len(res, num)
 }
