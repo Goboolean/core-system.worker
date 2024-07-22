709db3a5fc69f9b15238f6bc93c72c688d13fa45
orca <35363038+lsjtop10@users.noreply.github.com>
2024-07-22 14:48:13 +0900

    Refactor: refactor stop notifier test (#222)
    - introduce test suite

--- internal/util/stop_notifier_test.go
+++ internal/util/stop_notifier_test.go
@@ -4,89 +4,86 @@ import (
 	"testing"
 
 	"github.com/Goboolean/core-system.worker/internal/util"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestMain(m *testing.M) {
 	m.Run()
 }
 
-func TestStopNotifier(t *testing.T) {
-	t.Run("Done함수를 호출하지 않았을 때 sn.Done()은 흐름을 block해야 한다.", func(t *testing.T) {
+type StopNotifierTestSuite struct {
+	suite.Suite
+	sn *util.StopNotifier
+}
 
-		//arrange
-		//act
-		sn := util.NewStopNotifier()
+func (suite *StopNotifierTestSuite) SetupTest() {
+	suite.sn = util.NewStopNotifier()
+	suite.Require().NotNil(suite.sn)
+}
 
-		//assert
-		assert.NotNil(t, sn)
-		assert.NotNil(t, sn.Done())
-		select {
-		//If a channel returned by sn.Done() is not closed, default will be selected
-		case x := <-sn.Done():
-			t.Errorf("<-sn.Done() == %v, but it should block", x)
-		default:
-		}
+func (suite *StopNotifierTestSuite) TestDoneFunc_ShouldBlock_WhenNotifyStopIsNotCalled() {
+	//arrange
+	//act
+	//assert
+	suite.NotNil(suite.sn.Done())
+	select {
+	//If a channel returned by sn.Done() is not closed, default will be selected
+	case x := <-suite.sn.Done():
+		suite.Fail("<-sn.Done() == %v, but it should block", x)
+	default:
+	}
 
-	})
+}
 
-	t.Run("NotifyStop를 1회 호출했을때 sn.Done은 흐름을 block하지 않아야 한다.", func(t *testing.T) {
+func (suite *StopNotifierTestSuite) TestDoneFunc_ShouldNotBlock_WhenNotifyStopIsNotCalledOnce() {
+	//arrange
+	//act
+	suite.sn.NotifyStop()
+	//assert
 
-		//arrange
-		sn := util.NewStopNotifier()
-		//act
-		sn.NotifyStop()
-		//assert
-		assert.NotNil(t, sn)
-		assert.NotNil(t, sn.Done())
+	suite.NotNil(suite.sn.Done())
 
-		select {
-		//If a channel returned by sn.Done() is closed, the case <-sn.Done() will be selected
-		case <-sn.Done():
-		default:
-			t.Errorf("<-sn.Done() blocked, but it shouldn't block")
-		}
+	select {
+	//If a channel returned by sn.Done() is closed, the case <-sn.Done() will be selected
+	case <-suite.sn.Done():
+	default:
+		suite.Fail("<-sn.Done() blocked, but it shouldn't block")
+	}
 
-	})
+}
 
-	t.Run("NotifyStop를 2회(1회 초과) 호출했을때 sn.Done은 흐름을 block하지 않아야 한다.", func(t *testing.T) {
+func (suite *StopNotifierTestSuite) TestDoneFunc_ShouldNotBlock_WhenNotifyStopIsNotCalledMoreThenOnce() {
+	//arrange
+	//act
+	suite.sn.NotifyStop()
+	suite.sn.NotifyStop()
 
-		//arrange
-		sn := util.NewStopNotifier()
-		//act
-		sn.NotifyStop()
-		sn.NotifyStop()
-		//assert
-		assert.NotNil(t, sn)
-		assert.NotNil(t, sn.Done())
+	//assert
+	suite.NotNil(suite.sn.Done())
 
-		select {
-		//If a channel returned by sn.Done() is closed, the case <-sn.Done() will be selected
-		case <-sn.Done():
-		default:
-			t.Errorf("<-sn.Done() blocked, but it shouldn't block")
-		}
+	select {
+	//If a channel returned by sn.Done() is closed, the case <-sn.Done() will be selected
+	case <-suite.sn.Done():
+	default:
+		suite.Fail("<-sn.Done() blocked, but it shouldn't block")
+	}
+}
 
-	})
+func (suite *StopNotifierTestSuite) TestChannelRetrievedBeforeCallingNotifyStop_ShouldNotBlock_WhenNotifyStopIsCalled() {
+	//arrange
+	done := suite.sn.Done()
 
-	t.Run("NotifyStop을 호출했을 때 NotifyStop을 호출하기 전 Done에서 반환한 채널도 흐름을 block하지 않아야 한다.", func(t *testing.T) {
+	//act
+	suite.sn.NotifyStop()
 
-		//arrange
-		sn := util.NewStopNotifier()
-		done := sn.Done()
-		//act
-		sn.NotifyStop()
-		sn.NotifyStop()
-		//assert
-		assert.NotNil(t, sn)
-		assert.NotNil(t, sn.Done())
+	//assert
+	suite.NotNil(suite.sn.Done())
 
-		select {
-		//If a channel returned by sn.Done() is closed, the case <-sn.Done() will be selected
-		case <-done:
-		default:
-			t.Errorf("<-sn.Done() blocked, but it shouldn't block")
-		}
+	select {
+	//If a channel returned by sn.Done() is closed, the case <-sn.Done() will be selected
+	case <-done:
+	default:
+		suite.Fail("<-sn.Done() blocked, but it shouldn't block")
+	}
 
-	})
 }
